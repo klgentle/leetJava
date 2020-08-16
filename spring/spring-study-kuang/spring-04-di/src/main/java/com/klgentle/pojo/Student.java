fe0acf4b41a2c77e1f3ad7057369a797db545794
klgentle <klgentle@sina.com>
2020-08-16 18:03:40 +0800

    update folder, file re orgnaze

---
+++ spring/spring-study-kuang/spring-04-di/src/main/java/com/klgentle/pojo/Student.java
@@ -0,0 +1,22 @@
+package com.klgentle.pojo;
+
+public class Student {
+    private String Name;
+    private Address address;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
