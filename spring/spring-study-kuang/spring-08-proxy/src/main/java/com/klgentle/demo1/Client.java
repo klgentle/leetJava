5433e644e26b4a4baba905b437a4ad5dedf057d3
klgentle <klgentle@sina.com>
2020-08-19 18:44:23 +0800

    add 代理模式

--- spring/spring-study-kuang/spring-08-proxy/src/main/java/com/klgentle/demo1/Client.java
+++ spring/spring-study-kuang/spring-08-proxy/src/main/java/com/klgentle/demo1/Client.java
@@ -4,6 +4,8 @@ public class Client {
 
     public static void main(String[] args) {
         Host host = new Host();
-        host.rent();
+        // 代理
+        Proxy proxy = new Proxy(host);
+        proxy.rent();
     }
 }
