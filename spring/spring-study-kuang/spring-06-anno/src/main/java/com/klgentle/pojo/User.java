bce28ce6bc62c928f4621c7008496956143d53b6
klgentle <klgentle@sina.com>
2020-08-19 09:14:59 +0800

    update 使用注解开发

--- spring/spring-study-kuang/spring-06-anno/src/main/java/com/klgentle/pojo/User.java
+++ spring/spring-study-kuang/spring-06-anno/src/main/java/com/klgentle/pojo/User.java
@@ -1,11 +1,14 @@
 package com.klgentle.pojo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // 等价于 <bean id="user" class="..."/>
 @Component
+@Scope("prototype")
 public class User {
+
     @Value("klgentle")
     public String name;
 
