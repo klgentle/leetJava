7b518b09a44f4a7e9e593ef992aba5be2dfb542d
klgentle <kegentle@sina.com>
2020-09-02 23:01:45 +0800

    yaml 配置文件

---
+++ springboot/springboot-02-config2/src/test/java/com/gentle/config2/Config2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentle.config2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Config2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
